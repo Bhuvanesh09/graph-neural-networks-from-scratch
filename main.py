4016c1f261cde15815b91777f14730c8b697e6b1
bhuvanesh09 <bhuvan.sridharan@gmail.com>
2022-03-27 17:44:24 +0530

    GIN Experiment Added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import torch
 import pytorch_lightning as pl
 from tqdm import tqdm
 from gcn import Gcn
+from gin import Gin
 from torch.utils.data import Dataset, DataLoader
 
 
@@ -19,7 +20,7 @@ class GraphData(Dataset):
 
 df = pyg.datasets.Planetoid("datasets/planetoid", "citeseer")
 
-model = Gcn(2, 3703, 7, 7)
+
 
 # trainer = pl.Trainer(gpus=1)
 # data = df.data
@@ -36,6 +37,7 @@ def accuracy(probs, y):
 
 
 def train_gcn():
+    model = Gcn(2, 3703, 7, 7)
     optim = torch.optim.Adam(model.parameters(), lr=1e-2)
     loss_fun = torch.nn.CrossEntropyLoss()
 
@@ -51,8 +53,27 @@ def train_gcn():
                                                                       accuracy(out[train_mask], df.data.y[train_mask]),
                                                                       accuracy(out[val_mask], df.data.y[val_mask])))
         optim.step()
+        print("Train Loss:", float(loss))
 
-        print("Train Loss:", loss)
+def train_gin():
+
+    model = Gin(2, 3703, 7, 7)
+    optim = torch.optim.Adam(model.parameters(), lr=1e-2)
+    loss_fun = torch.nn.CrossEntropyLoss()
+
+    for ep in tqdm(range(100)):
+        model.train()
+        optim.zero_grad()
+        out = model(df.data.x, df.data.edge_index.T)
+        train_mask, val_mask = df.data.train_mask, df.data.val_mask
+        loss = loss_fun(out[train_mask], df.data.y[train_mask])
+        loss.backward()
+
+        print("Epoch {}, Train Accuracy: {}, Val Accuracy: {}".format(ep,
+                                                                      accuracy(out[train_mask], df.data.y[train_mask]),
+                                                                      accuracy(out[val_mask], df.data.y[val_mask])))
+        optim.step()
+        print("Train Loss:", float(loss))
 
 if __name__== "__main__":
-    train_gcn()
+    train_gin()
